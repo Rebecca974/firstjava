b34e4750e216c93fbbcbb12d06e4c75293cbfa60
rebecca136 <54754930+rebecca136@users.noreply.github.com>
2020-06-23 18:36:15 -0400

    add to my existing code

--- firstjava/src/main/java/com/aipalbot/firstjava/App.java
+++ firstjava/src/main/java/com/aipalbot/firstjava/App.java
@@ -1,5 +1,7 @@
 package com.aipalbot.firstjava;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,56 +11,49 @@ public class App
     public static void main( String[] args )
     { 
     	
-    	int totalPrice;
-    	int quantity = 3;
-    	int costPerItem = 7;
-    	int discountCheck = 17;
-    	totalPrice = quantity * costPerItem;
     	
-    	System.out.println("before discount" + totalPrice);
-    	
-    	if(totalPrice > discountCheck) {
-    		int newTotal =totalPrice;
-    		newTotal -= costPerItem;
-    		
-    		//totalPrice = totalPrice - costPerItem;
-    		
-    		System.out.println("after discount " + newTotal);
-    	}
-    	
-    	
-    	//System.out.println("after discount " + totalPrice);
-    	//I am a programmer
-       /* This is my first programming and I glad to face any challenging that is coming my way*/
-    	
-    	//System.out.println( "Welcome to Java Programing!" );
+    
+     double totalPrice;
+    double percentage =10;
+    
    
+    Scanner scanner = new Scanner(System.in);
     
-  //Assignment
-    /* 
-     * Calculate the total price of 8 items at $3 each and subtract the cost of 2 items from the total price, 
-     * if total price is more than $20
-     * Display the amount to be paid after discount
-     */
+   System.out.println("Enter quantity:");
+    int quantity = scanner.nextInt();
+    System.out.println("Enter value for quantity: " + quantity);
+    System.out.println("Enter Price :");
+    int price = scanner.nextInt();
+    System.out.println("Enter value for Price: " + price);
     
-    int totalPrice1;
-    int item = 8;
-    int price = 3;
-    int discount = 20;
-    
-    
-    totalPrice1 = item * price;
-    System.out.println("The totalPrice is for my first project:" + totalPrice1);
-    
+   totalPrice = quantity* price;
+    System.out.println("The totalPrice is:" + totalPrice);
      
-    if (totalPrice1> discount) {
-    	
-    	totalPrice1-= price + price;	
-    	
-    		
-    		System.out.println("The totalPrice after discount is: " + totalPrice1);
-    	}
+    double discount = (percentage/100.00) * totalPrice;
+     
+   System.out.println(" discount is: " + discount);
     
+   
+   Scanner scanner2 = new Scanner(System.in);
+   
+    System.out.println("Enter your Promocode:");
+  String promoCode = scanner2.nextLine();
+    System.out.println(" your promocode : \n" + promoCode);	
+    	
     
+    scanner2.close();
+    	scanner.close();
+    	
+    if(totalPrice > 200.00 && promoCode.equals("Summer2020")) {
+    	totalPrice = totalPrice-discount;
+    }
+     
+   
+   System.out.println(totalPrice);
+    	
+    }
+    
+   
 }
-}
+ 
+	
